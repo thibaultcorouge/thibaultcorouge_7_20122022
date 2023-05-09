2e669eece7a9cda4b6eb6e32fa0d1b0b25f58ff5
thibault corouge <thibaultcorouge@MacBook-Air-de-thibault.local>
2023-05-09 21:54:28 +0200

    adding comments about page

--- myapp/src/pages/About.jsx
+++ myapp/src/pages/About.jsx
@@ -1,17 +1,17 @@
 import AboutPage from "../components/About";
-import Collapse from "../components/Collapse";
+import Collapse from "../components/Collapse"; 
 import aboutArray from "../about-array.json"; 
 
-export default function About() {
+export default function About() { // Define the About component
 	return (
 		<>
-			<AboutPage/>
-			{aboutArray.map((rule, id) => (
+			<AboutPage/> {/* Render the AboutPage component */}
+			{aboutArray.map((rule, id) => ( // Loop through the aboutArray data and render the Collapse component for each item
 				<Collapse
-					key={id}
-					aboutTitle={rule.aboutTitle}
-					aboutText={rule.aboutText}
-					aboutStyle="about-style"
+					key={id} // Set the key prop to the id of the item
+					aboutTitle={rule.aboutTitle} // Set the aboutTitle prop to the aboutTitle property of the item
+					aboutText={rule.aboutText} // Set the aboutText prop to the aboutText property of the item
+					aboutStyle="about-style" // Set the aboutStyle prop to "about-style"
 				/>
 			))}
 		</>
