14b857a8b1ac526ccbddd65395c0882916411e4b
thibault corouge <thibaultcorouge@MacBook-Air-de-thibault.local>
2023-05-09 20:50:50 +0200

    modifying some dependencies and adding routes

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Header from "./components/Header";
+import Housing from "./pages/Housing";
+import Footer from "./components/Footer";
+import Error from "./pages/Error.jsx";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<Header />
+			<main>
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/about" element={<About />} />
+					<Route path="/housing/:id" element={<Housing />} />
+					<Route path="*" element={<Error />} />
+				</Routes>
+			</main>
+			<Footer />
+		</div>
+	);
 }
 
 export default App;
