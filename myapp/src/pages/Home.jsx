6a2bf1d0bcdec71fae0a26541b158aaaf68837b2
thibault corouge <thibaultcorouge@MacBook-Air-de-thibault.local>
2023-05-09 21:48:21 +0200

    adding comments Pages/Home.jsx

--- myapp/src/pages/Home.jsx
+++ myapp/src/pages/Home.jsx
@@ -4,9 +4,11 @@ import Card from "../components/Cards";
 import { Link } from "react-router-dom";
 
 export default function Home() {
+  // Define state to store data received from the server
   const [data, setData] = useState([]);
 
   useEffect(() => {
+    // Fetch data from server when the component mounts
     fetch("/housing.json")
       .then((response) => response.json())
       .then((data) => setData(data))
@@ -15,10 +17,13 @@ export default function Home() {
 
   return (
     <>
+      {/* Render a banner */}
       <Banner />
       <div className="cards-container">
+        {/* Map over the data and render cards for each */}
         {data.map((appart, id) => (
           <div className="card_logement" key={id}>
+            {/* Add a link to each card to navigate to the housing detail page */}
             <Link className="link_card_logement" to={`/housing/${appart.id}`}>
               <Card cover={appart.cover} title={appart.title} />
             </Link>
