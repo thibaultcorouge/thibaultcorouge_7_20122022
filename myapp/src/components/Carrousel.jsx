dbf4877bc530ffaf51b5777d5a3162e86c6fe08a
thibault corouge <thibaultcorouge@MacBook-Air-de-thibault.local>
2023-05-25 18:45:09 +0200

    modifying code to hide slidenumber when there's only 1 photo

--- myapp/src/components/Carrousel.jsx
+++ myapp/src/components/Carrousel.jsx
@@ -3,59 +3,59 @@ import left from "../assets/arrow-left.svg";
 import right from "../assets/arrow-right.svg";
 
 export default function Carrousel({ slides }) {
-	// Set initial state with useState hook
-	const [current, setCurrent] = useState(0); 
-	const length = slides.length; 
+  // Set initial state with useState hook
+  const [current, setCurrent] = useState(0);
+  const length = slides.length;
 
-	// Define functions to handle next and previous slide
-	const nextSlide = () => {
-		setCurrent(current === length - 1 ? 0 : current + 1); 
-	};
-	const prevSlide = () => {
-		setCurrent(current === 0 ? length - 1 : current - 1); 
-	};
+  // Define functions to handle next and previous slide
+  const nextSlide = () => {
+    setCurrent(current === length - 1 ? 0 : current + 1);
+  };
+  const prevSlide = () => {
+    setCurrent(current === 0 ? length - 1 : current - 1);
+  };
 
-	return (
-		// Render the carousel container
-		<section id="carrousel-container">
-			{/* Render the left arrow if there is more than one slide */}
-			{length > 1 && (
-				<img
-					src={left}
-					alt="gauche"
-					onClick={prevSlide}
-					className="leftArrow"
-				/>
-			)}
-			{/* Render the right arrow if there is more than one slide */}
-			{length > 1 && (
-				<img
-					src={right}
-					alt="droite"
-					onClick={nextSlide}
-					className="rightArrow"
-				/>
-			)}
-			{/* Render each slide with a slider class, and add the active-anim class to the current slide */}
-			{slides.map((slide, index) => (
-				<div
-					key={index} 
-					className={
-						current === index
-							? "slider bl-msk wh-msk active-anim"
-							: "slider bl-msk wh-msk"
-					}
-				>
-					{/* Render the slide image only if it is the current slide */}
-					{index === current && <img src={slide} alt="appartement à louer" />}
-					{/* Render the slide number only if it is the current slide */}
-					{index === current && (
-						<span className="slider__number">
-							{current + 1}/{length}
-						</span>
-					)}
-				</div>
-			))}
-		</section>
-	);
+  return (
+    // Render the carousel container
+    <section id="carrousel-container">
+      {/* Render the left arrow if there is more than one slide */}
+      {length > 1 && (
+        <img
+          src={left}
+          alt="gauche"
+          onClick={prevSlide}
+          className="leftArrow"
+        />
+      )}
+      {/* Render the right arrow if there is more than one slide */}
+      {length > 1 && (
+        <img
+          src={right}
+          alt="droite"
+          onClick={nextSlide}
+          className="rightArrow"
+        />
+      )}
+      {/* Render each slide with a slider class, and add the active-anim class to the current slide */}
+      {slides.map((slide, index) => (
+        <div
+          key={index}
+          className={
+            current === index
+              ? "slider bl-msk wh-msk active-anim"
+              : "slider bl-msk wh-msk"
+          }
+        >
+          {/* Render the slide image only if it is the current slide */}
+          {index === current && <img src={slide} alt="appartement à louer" />}
+          {/* Render the slide number only if it is the current slide and there are more than one slide */}
+          {length > 1 && index === current && (
+            <span className="slider__number">
+              {current + 1}/{length}
+            </span>
+          )}
+        </div>
+      ))}
+    </section>
+  );
 }
